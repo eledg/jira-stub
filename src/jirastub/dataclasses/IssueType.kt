08a807a05adc60eac74ad7b3da241dea1202a44c
Joel McNamara <jmmcnamara@uk-c02yv22flvdm.uk.deloitte.com>
2019-09-09 10:36:06 +0100

    created IssueType data class

---
+++ src/jirastub/dataclasses/IssueType.kt
@@ -0,0 +1,11 @@
+package com.elliotledger.jirastub.dataclasses
+
+data class IssueType(
+    val self: String,
+    val id: String,
+    val description: String,
+    val iconUrl: String,
+    val name: String,
+    val subtask: Boolean,
+    val avatarId: Int
+)
