5eb5d4d3c0a9896789a1e5154aa13ae9177ad5ea
Joel McNamara <jmmcnamara@uk-c02yv22flvdm.uk.deloitte.com>
2019-09-09 16:20:19 +0100

    added initial test for GetSearchResultWith function

--- test/JiraStubTest.kt
+++ test/JiraStubTest.kt
@@ -36,4 +36,17 @@ class JiraStubTest {
             }
         }
     }
+    @Test
+    fun testGetSearchResultWith() {
+        withTestApplication({ module(testing = true)}) {
+            handleRequest(HttpMethod.Get, "/rest/api/2/search?jql=test") {
+                addHeader(
+                    HttpHeaders.Authorization,
+                    HttpAuthHeader.Single("basic", Base64.getEncoder().encodeToString("$username:$password".toByteArray())).render()
+                )
+            }.apply {
+                assertEquals(HttpStatusCode.OK, response.status())
+            }
+        }
+    }
 }
