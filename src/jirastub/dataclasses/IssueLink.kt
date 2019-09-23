5e312bd9ed30f19c49202bae42a1fc3a3c92e6df
Joel McNamara <jmmcnamara@uk-c02yv22flvdm.uk.deloitte.com>
2019-09-23 09:42:49 +0100

    Changed var to val in issue endpoint

--- src/jirastub/dataclasses/IssueLink.kt
+++ src/jirastub/dataclasses/IssueLink.kt
@@ -3,5 +3,5 @@ package com.elliotledger.jirastub.dataclasses
 data class IssueLink(
     val type: IssueLinkType,
     val outwardIssue: OutwardInwardIssue,
-    val inwardissue: OutwardInwardIssue
+    val inwardIssue: OutwardInwardIssue
 )
